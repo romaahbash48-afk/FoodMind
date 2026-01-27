2edd129da22587f8b99acf61f7ae9d3be25efc60
Roman <ahbashroman1028@gmail.com>
2026-01-27 19:50:34 +0100

    baseline: project builds and runs

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.4")
+        classpath("com.android.tools.build:gradle:8.12.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
     }
